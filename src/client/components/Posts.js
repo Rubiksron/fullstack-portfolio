a800124caa0b4c836e62006083ecaa848ae3aefd
Rubiksron <rubiksron@gmail.com>
2018-11-09 15:15:39 -0800

    added another API call so that I can display the results of two different calls in an overflow: scroll styled  <div>.

--- src/client/components/Posts.js
+++ src/client/components/Posts.js
@@ -1,9 +1,5 @@
 import React, { Component } from 'react';
 
-const POKEMON_API = 'https://pokeapi.co/api/v2/pokemon/';
-const JSONPLACEHOLDER_API = 'https://jsonplaceholder.typicode.com/posts';
-
-
 class Posts extends Component {
   constructor(props) {
     super(props);
@@ -12,26 +8,23 @@ class Posts extends Component {
     };
   }
 
-  componentWillMount() {
-    fetch(POKEMON_API)
+componentWillMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
       .then(res => res.json())
-      .then(data => this.setState({ posts: data.results }))
-      .then(posts => console.log('API_DATA: ', this.state.posts[1]));
+      .then(data => this.setState({ posts: data }));
   }
 
   render() {
-    const pokemon = this.state.posts.map((post, i) => (
-      <ol key={i}>
-        <h3>name:</h3>
-        <h2>{post.name}</h2>
-        <br />
-        <a href={post.url}>abilities</a>
-        <hr />
-      </ol>
+    const postItems = this.state.posts.map(post => (
+      <div key={post.id}>
+        <h3>{post.title} - {post.id}</h3>
+        <p>{post.body}</p>
+      </div>
     ));
     return (
       <div>
-        { pokemon }
+        <h1>Posts:</h1>
+        {postItems}
       </div>
     );
   }
